b5c7e90247bcba3597cdd28714010af23cdd3e84
globalyck <18810276930@163.com>
2017-09-24 17:11:41 +0800

    request search succeed.don't know the encoding problem.

--- crawl_info/craw163/craw163/spiders/search_artist.py
+++ crawl_info/craw163/craw163/spiders/search_artist.py
@@ -1,9 +1,8 @@
 # TODO 20170916 search artist ID by giving artist name
 # Full request URI: http://music.163.com/api/artist/albums/166009?id=166009&offset=0&total=true&limit=5
-# TEST artist:eminem(32665),tupac,snoop dog,BIG,nas,50cent
+# TEST artist:eminem(32665),tupac,snoop doG,nas,50cent
 import scrapy
 from scrapy.utils.response import open_in_browser
-
 
 class searchArtist(scrapy.Spider):
     name = "search_artist"
@@ -12,15 +11,22 @@ class searchArtist(scrapy.Spider):
         search_words = {"Eminem", "Tupac", "gai", "马思唯"}
         for search_word in search_words:
             url = "http://music.163.com/api/search/get/"
-            data = {"s": search_word, "limit": 1, "type": 100}
+            data = {"s": search_word, "limit": "1", "type": "100"}
             url2 = "http://music.163.com/api/search/get/?s=gai&limit=1&type=100"
-            # TODO 20170916 Can not make a post request.In lib requests it works,using Postman get 403 forbidden error.
-            # TODO 20170922 Find out the relationship between Request Object and Response,how yield work.
-            yield scrapy.Request(url=url, method="POST", body=str(data),
-                                 callback=self.parse, cookies={"appver": "1.5.2"})
+            # TODOdone 20170922 Find out the relationship between Request Object and Response,how yield work.
+            # yield scrapy.Request(url=url, method="POST", body=str(data),
+            #                      callback=self.parse, cookies={"appver": "1.5.2"})
+            # TODO why this work??
+            yield scrapy.FormRequest(url=url, formdata=data)
 
     def parse(self, response):
         open_in_browser(response)
-        page = response.body
-        yield {"content": response}
-        print(page)
+        print(response.encoding)
+        # TODO why this work??ref:https://stackoverflow.com/questions/9181214/scrapy-text-
+        # python 2.x:
+        # unicode(response.body.decode(response.encoding)).encode('utf-8')
+        # python 3.x:
+        page = str((response.body.decode(response.encoding)).encode('utf-8'), 'utf-8')
+        # print(page)
+        yield {"content": page}
+
