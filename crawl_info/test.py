3e4db2f2907edf675d27d9a8d005913e82467df6
forrestlmj <18810276930@163.com>
2018-08-04 22:52:19 +0800

    test尝试可以得到链接

--- crawl_info/test.py
+++ crawl_info/test.py
@@ -12,7 +12,9 @@ headers = {
     "Upgrade-Insecure-Requests": "1"
 }
 # "Cookie":"JSESSIONID-WYYY=oAkTXXbzyu%2F9G9HAuVpiHTxa%2BxDJMiG2VU5CnU3vCcGFNwfuVOYyK5MXBXPKREiw8Hy4WBxzA7p3sRrNsKuDC8Qy5IJYxkYTsKA%2FXANVJf2T%5C05Sjgz1tS00ABpolIJ0et%2BjC4AJOwvt1%5Cc9PShGfNCAvnSP7i%5CsEfvUIpmriDTRcRf5%3A1533392526074; _iuqxldmzr_=32; _ntes_nnid=05b00c1b3c2c5fc4f2c9d259e432dfa4,1533390726092; _ntes_nuid=05b00c1b3c2c5fc4f2c9d259e432dfa4; __utma=94650624.1551482594.1533390728.1533390728.1533390728.1; __utmb=94650624.17.10.1533390728; __utmc=94650624; __utmz=94650624.1533390728.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); WM_NI=SCno3iMUr%2BwhY3KBA%2BTwo1nuF3POEPEXLmlWiuWuDnPE3DZfCjhVrRX4ejw0%2Fj7MOOJK7wCUPhwoq1hkjJh0v3FHTftNvPApAyoSOZ%2FbACCQUy6DKzh1azSnZk3vPKWnVzc%3D; WM_NIKE=9ca17ae2e6ffcda170e2e6eea9d77cfbba8491e66b8986f8abd86892f09db3c646a3edff9bd442b69e8299b72af0fea7c3b92a919da290b2638f8bbeccf6649bed85a3c42198a9a1d2ed7f86eda0b0c85c8788848eef5dabab9996c766949bfda5d96e81b6a8b7f15a9cecab8bf63cedb6f9aece458a95f998c25f94b8b990f43bacb5a6abeb3cb6ed8aa9d76eb892bab1c47c83a7a1b4ce52edbe8fa4c25b97a7faa5ca72acefaf9bd2669ab5fd97d34195b29a9be237e2a3; WM_TID=XYoF9tZSzJZTpQPdCvFKVZOSBVvAQiW%2B",
-url = "https://music.163.com/discover/artist/cat?id=2001"
-r = re.get(url=url, headers=headers)
+url_hot = "https://music.163.com/discover/artist/cat?id=2001"
+# 注意url中discover/#/artist部分要去掉＃
+url_page_F = "https://music.163.com/discover/artist/cat?id=2001&initial=70"
 
-print(r.text)
+# print(re.get(url=url_hot, headers=headers).text)
+print(re.get(url=url_page_F, headers=headers).text)
